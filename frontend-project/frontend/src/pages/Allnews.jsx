69488e4705266a490f4528bac1202b8383af7280
Lakshman Gannabathula <your-email@example.com>
2025-12-31 12:14:56 +0530

    Fix API URL in real frontend

--- frontend-project/frontend/src/pages/Allnews.jsx
+++ frontend-project/frontend/src/pages/Allnews.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useMemo, useState } from "react";
-
 const API_URL =
     window.location.hostname === "localhost"
         ? "http://127.0.0.1:8000/api/live-news/"
         : "https://ca-website-qj5u.onrender.com/api/live-news/";
+
 
 const PAGE_SIZE = 10;
 
