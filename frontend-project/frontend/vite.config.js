219bba35f70ca8653c9aa4e2ad955988800b8ea7
Lakshman Gannabathula <your-email@example.com>
2026-01-01 17:04:01 +0530

    fix: vite base path for vercel (from /static to /)

--- frontend-project/frontend/vite.config.js
+++ frontend-project/frontend/vite.config.js
@@ -3,13 +3,10 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/static/",
+  base: "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
   },
 })
-
-
-
 
